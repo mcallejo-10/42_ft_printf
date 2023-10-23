8e82a7aabe68600c7e4d124b1a2944b3050390de
mcallejo-10 <mcallejo@student.42barcelona.com>
2023-10-23 15:38:44 +0200

    dilluns migdia

--- ft_print_hex.c
+++ ft_print_hex.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_int.c                                     :+:      :+:    :+:   */
+/*   ft_print_hex.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcallejo <mcallejo@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/20 20:48:21 by mcallejo          #+#    #+#             */
-/*   Updated: 2023/10/23 15:34:24 by mcallejo         ###   ########.fr       */
+/*   Created: 2023/10/23 15:09:02 by mcallejo          #+#    #+#             */
+/*   Updated: 2023/10/23 15:35:14 by mcallejo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libprintf.h"
 
-static int	ft_nlen(int n)
+static int	ft_nlen(char c, int n)
 {
 	int					nlen;
 	long long int		ntemp;
@@ -28,31 +28,41 @@ static int	ft_nlen(int n)
 	}
 	while (ntemp > 0)
 	{
-		ntemp = ntemp / 10;
+		ntemp = ntemp / 16;
 		nlen++;
 	}
 	return (nlen);
 }
 
-int	ft_print_int(int di)
+int	ft_print_hex(int x)
 {
 	long int	nlong;
 	int			j;
 
 	j = 0;
-	nlong = (long int)di;
-	if (di< 0)
+	nlong = (long int)x;
+	if (x < 0)
 	{
 		nlong = nlong * -1;
 		ft_putcharcounter('-');
 	}
-	if (nlong > 9)
+    if (nlong >= 16)
 	{
-		ft_print_int(nlong / 10);
-		ft_putcharcounter(nlong % 10 + '0');
+		ft_print_int(nlong / 16);
+		ft_putcharcounter(nlong % 16 + '0');
 		j++;
 	}
-	else if (nlong < 10)
-		ft_putcharcounter(nlong + '0');
-	return (ft_nlen(di));
+    else
+    {
+        if (nlong <= 9)
+        ft_putcharcounter(nlong + '0')
+    }
+	    else if (nlong < 16)
+        {
+            if (c == 'x')
+		        ft_putcharcounter(nlong - 10 + 'a');
+            else if (c == 'X')
+                ft_putcharcounter(nlong - 10 + 'A');
+        }
+	return (ft_nlen(x));
 }
